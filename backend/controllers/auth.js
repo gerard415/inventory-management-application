abacef8c5d8c44d89c172f2d244ad05c2f89abf7
Chinedum <chinedumike85@gmail.com>
2022-12-11 16:31:35 +0100

    hashing the password using mongoose middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,10 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+
 const register = async (req, res) => {
-    res.send('register user')
+    const user = await User.create({...req.body})
+    res.status(200).json({user})
 }
 
 const login = async (req, res) => {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -21,6 +22,12 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'please provide a password'],
         minlength: 8
     }
+})
+
+//hashing the password using mongoose middleware
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt) 
 })
 
 module.exports = mongoose.model('User', UserSchema)

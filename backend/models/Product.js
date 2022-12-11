a0026b2ef1883a61149f7bd7cdcae7fb5dc4200e
Chinedum <chinedumike85@gmail.com>
2022-12-11 15:19:06 +0100

    setting up the models

---
+++ backend/models/Product.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'please provide a product name'],
+        maxlength: 20,
+        minlength: 3
+    },
+    category: {
+        type: String,
+        required: [true, 'please provide a category'],
+        maxlength: 20,
+        minlength: 3
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'please provide the quantity']
+    },
+    price: {
+        type: Number,
+        required: [true, 'please provide the price']
+    },
+    description: {
+        type: String,
+        required: [true, 'please provide the description'],
+        trim: true
+    },
+    image: {
+        type: Object,
+        default: {}
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide the user'],
+      },
+}, 
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please provide a name'],
+        maxlength: 50,
+        minlength: 4
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide an email'], 
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+          ],
+          unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide a password'],
+        minlength: 8
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)

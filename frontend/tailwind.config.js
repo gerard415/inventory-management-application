41eb76fbfe9f50ddf3589941be4409d74eaaebe7
gerard415 <chinedumike85@gmail.com>
2023-10-19 12:34:56 +0100

    installations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
@@ -9,7 +10,15 @@ module.exports = {
       'lato': ['lato', 'sans-serif'],
       'poppins': ['Poppins', 'sans-serif'],
       'roboto': ['Roboto Condensed', 'sans-serif']
-    }
+    },
+    screens: {
+      'phone': '370px',
+      'sm':'640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl':'1280px',
+      '2xl':'1536px'
+    },
   },
-  plugins: [],
+  plugins: [ require('flowbite/plugin')],
 }
